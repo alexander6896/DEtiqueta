6ef3d04c59763b8b1a9bfad80549d9a2faa2f78b
alexa <alexa@DESKTOP-NPLTT9T>
2021-10-09 21:32:22 -0500

    Agregar archivos de proyecto.

---
+++ BusinessModel/Business/PruebaBL.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessModel.Business
+{
+    public class PruebaBL
+    {
+        public string Nombre()
+        {
+            return "Alexander Vladimir Muñiz Escobedo";
+        }
+    }
+}
---
+++ DEtiqueta/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BusinessModel.Business;
+
+namespace DEtiqueta.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            PruebaBL oPruebaBL = new PruebaBL();
+            string msj = oPruebaBL.Nombre();
+
+            ViewBag.Message = msj;
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
